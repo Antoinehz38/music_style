ea1b0d3dda012053596a296608298b63f75d6d59
antoine <antoine.hanachowicz@outlook.com>
2025-12-02 21:14:29 +0100

    new tests

--- src/train.py
+++ src/train.py
@@ -114,8 +114,8 @@ if __name__ == "__main__":
     args = parse_args()
     print('baseline = ', args.baseline)
     if args.run_from == "new_conf":
-        config_run = RunSummary(random_crop=True, model_type="CRNN", dataset_type="NewMelNpyDataset",optim_type="AdamW",
-                                target_T=256, seed=seed, batch_size=32, lr=0.0003, weight_decay=0.0001, epoch=35)
+        config_run = RunSummary(random_crop=True, model_type="CRNN", dataset_type="MelNpyDataset",optim_type="AdamW",
+                                target_T=512, seed=seed, batch_size=32, lr=0.0003, weight_decay=0.0001, epoch=35)
     else:
         config_run = RunSummary()
         with open(args.run_from, "r", encoding="utf-8") as f:
