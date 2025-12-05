da6002c6d58ca7e56c3b30de459432c544578d53
antoine <antoine.hanachowicz@outlook.com>
2025-12-05 17:22:27 +0100

    for CRNN

--- src/train.py
+++ src/train.py
@@ -118,7 +118,7 @@ if __name__ == "__main__":
     print('baseline = ', args.baseline)
     if args.run_from == "new_conf":
         config_run = RunSummary(random_crop=True, model_type="CRNN", dataset_type="MelNpyDataset",optim_type="AdamW",
-                                target_T=1292, seed=seed, batch_size=32, lr=0.0003, weight_decay=0.0001, epoch=35, val_training=False)
+                                target_T=1292, seed=seed, batch_size=32, lr=0.001, weight_decay=0.0001, epoch=20, val_training=False)
     else:
         config_run = RunSummary()
         with open(args.run_from, "r", encoding="utf-8") as f:
